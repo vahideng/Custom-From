637780304c590abdab470443e8a4e0643ede9e84
Vahid Davoudi <vahid_davoudi_e@yahoo.com>
2018-10-26 20:28:35 +0800

    add Coustom input element

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,63 +1,54 @@
-import React, {
-    Component
-} from 'react'
+import React, { Component } from 'react';
 
 export default class Form extends Component {
+  state = {
+    orderForm: {
+      name: {
+        elementType: 'input',
+        config: {
+          placeholder: 'Your Name',
+          type: 'text'
+        },
+        value: ''
+      },
 
-    state = {
-
-        orderForm: {
-            name: {
-                elementType: "input",
-                config: {
-                    placeholder: "Your Name",
-                    type: "text"
-                },
-                value: ''
-            },
-
-            address: {
-                elementType: "textarea",
-                config: {
-                    placeholder: "Your address",
-                    type: "text"
-                },
-                value: ''
-            },
-            email: {
-                elementType: "input",
-                config: {
-                    placeholder: "Your address",
-                    type: "email"
-                },
-                value: ''
-            },
-            delivery: {
-                elementType: "select",
-                config: {
-                    options: [
-                        { value: 'fastest', displayValue: 'Fastest' },
-                        { value: 'chepest', displayValue: 'Cheapest' }
-                    ],
-                    type: "text"
-                },
-                value: ''
-
-            }
-        }
-
+      address: {
+        elementType: 'textarea',
+        config: {
+          placeholder: 'Your address',
+          type: 'text'
+        },
+        value: ''
+      },
+      email: {
+        elementType: 'input',
+        config: {
+          placeholder: 'Your address',
+          type: 'email'
+        },
+        value: ''
+      },
+      delivery: {
+        elementType: 'select',
+        config: {
+          options: [
+            { value: 'fastest', displayValue: 'Fastest' },
+            { value: 'chepest', displayValue: 'Cheapest' }
+          ],
+          type: 'text'
+        },
+        value: ''
+      }
     }
+  };
 
-
-
-
-    render() {
-        return (<div >
-            <form >
-                <input type="text"
-                    placeholder="yourname" />
-            </form>
-        </div >
-        )
-    }
+  render() {
+    return (
+      <div>
+        <form>
+          <input type="text" placeholder="yourname" />
+        </form>
+      </div>
+    );
+  }
 }

ef79962c572b71c8215cdce67617f129215c2eef
Vahid Davoudi <vahid_davoudi_e@yahoo.com>
2018-10-26 12:44:12 +0800

    construct form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Form from "./Form/Form";
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-         
-        </header>
+        <Form/>
       </div>
     );
   }
